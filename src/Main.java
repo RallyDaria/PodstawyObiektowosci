fcd96550cdbcf73926564f9466c11efdc173e8ad
RallyDaria <dariazblewska@gmail.com>
2022-10-26 19:29:43 +0200

    Rabbit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Rabbit rabbit1=new Rabbit(25, "Heniek",true);
+    }
+}
---
+++ src/Rabbit.java
@@ -0,0 +1,11 @@
+public class Rabbit {
+    int lenght;
+    String name;
+    boolean  softness;
+
+    Rabbit(int lenght,String name, boolean softness){
+        this.lenght=lenght;
+        this.name=name;
+        this.softness=softness;
+    }
+}
