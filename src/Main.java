39eec0f2ca38408490bce6bd258b761aa3bf9a5d
RallyDaria <dariazblewska@gmail.com>
2022-10-26 19:58:46 +0200

    Zmiana imienia Heńka

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         Rabbit rabbit1=new Rabbit(25, "Heniek",true);
+        rabbit1.changeName("Kiziol");
     }
 }
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -1,11 +1,16 @@
 public class Rabbit {
-    int lenght;
-    String name;
-    boolean  softness;
+
+    private int lenght;
+    private String name;
+    private boolean  softness;
 
     Rabbit(int lenght,String name, boolean softness){
         this.lenght=lenght;
         this.name=name;
         this.softness=softness;
     }
+    public void changeName(String name){
+        this.name=name;
+    }
+
 }
