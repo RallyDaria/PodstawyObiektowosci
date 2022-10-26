805125f8278443417f29529ad8a9dbb93efab235
RallyDaria <dariazblewska@gmail.com>
2022-10-26 21:42:44 +0200

    Rabbit zapamiętaj po zmianach automatyczny getset

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         Rabbit rabbit1=new Rabbit(25, "Heniek",true);
-        rabbit1.changeName("Kiziol");
-        rabbit1.viewName();
-        System.out.println(rabbit1.viewName());
-        rabbit1.changeSoftnes(false);
-        System.out.println(rabbit1.viewSoftness());
+        rabbit1.setName("Kiziol");
+        rabbit1.getName();
+        System.out.println(rabbit1.getName());
+        rabbit1.setSoftness(false);
+        System.out.println(rabbit1.isSoftness());
+        rabbit1.setLenght(30);
+        System.out.println(rabbit1.getLenght());
+
     }
 }
--- src/Rabbit.java
+++ src/Rabbit.java
@@ -2,22 +2,36 @@ public class Rabbit {
 
     private int lenght;
     private String name;
-    private boolean  softness;
+    private boolean softness;
 
-    Rabbit(int lenght,String name, boolean softness){
-        this.lenght=lenght;
-        this.name=name;
-        this.softness=softness;
+    public Rabbit(int lenght, String name, boolean softness) {
+        this.lenght = lenght;
+        this.name = name;
+        this.softness = softness;
     }
-    public void changeName(String name){
-        this.name=name;
+
+    public int getLenght() {
+        return lenght;
     }
-    public String viewName(){
+
+    public String getName() {
         return name;
     }
-    public void changeSoftnes(boolean softness){
-        this.softness=softness;
+
+    public boolean isSoftness() {
+        return softness;
     }
-    public boolean viewSoftness(){
-        return softness;}
+
+    public void setLenght(int lenght) {
+        this.lenght = lenght;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSoftness(boolean softness) {
+        this.softness = softness;
+    }
+
 }
